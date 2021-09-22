0ef0174f9dc0b7ad83d58250a779b112018fcb16
angelortizv <angelo2898@gmail.com>
2021-09-22 17:33:27 -0600

    [ADD] Set Netlify functions

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-let adminClient = require('@keycloak/keycloak-admin-client').default;
-let Issuer = require('openid-client');
-var issuer = Issuer.Issuer;
-
-const express = require('express');
+const express = require("express");
+const serverless = require("serverless-http");
 const cors = require('cors');
 const songsRouter = require('./routes/songs');
-const { set } = require('mongoose');
-
-const {login} = require('./components/loginComponent');
-
-const PORT = process.env.PORT || 5000;
 
 const app = express();
+const router = express.Router();
 app.use(express.json());
 app.use(cors({origin: 'http://localhost:3000'}));
 
@@ -23,10 +16,6 @@ app.post('*', (req, res) => { res.status(405).send('Method does not exist'); });
 app.put('*', (req, res)  => { res.status(405).send('Method does not exist'); });
 app.delete('*', (req, res)  => { res.status(405).send('Method does not exist'); });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+app.use('/', router);
 
-//findSong({songName:'Casin',songAuthor:'glue70'});
-//createSong({songName:'Mr Blue Sky', songAuthor:'ELO', songAlbum:'Out Of The Blue', songLyrics:"Hey hey mr blue", creationAuthor:'Agustin', creationDate:'11/09/2021', modificationAuthor:'NA', modificationDate:'NA'})
-//updateSong({ songName:'Casin', songAuthor:'glue70'},{modificationAuthor:'Momboñombo Moñagallo'});
-//deleteSong({songName:'Mr Blue Sky', songAuthor:'ELO'});
-//login(); 
+module.exports.handler = serverless(app);
