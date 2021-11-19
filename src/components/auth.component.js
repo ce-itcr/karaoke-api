71af69054ee4b10f94395cd6a3f8514dd293ae41
angelortizv <angelo2898@gmail.com>
2021-11-19 12:32:44 -0600

    [ADD] updatePlayedSongs

--- src/components/auth.component.js
+++ src/components/auth.component.js
@@ -3,7 +3,6 @@ const { isCorrectPassword } = require('../shared/auth/auth.functions');
 
 
 const verifyUser = (req, res) => {
-    console.log(req.body)
     const { userId, password } = req.body;
 
     const databaseConnection = getConnection();
@@ -26,7 +25,6 @@ const verifyUser = (req, res) => {
 
 const getSingleUser = (req, res) => {
     let userId = req.params.userId;
-    console.log(userId)
     const databaseConnection = getConnection();
     databaseConnection.collection("users").findOne({"userId": userId}, { projection: { _id:0 } }, 
         function(error, data) {
@@ -58,6 +56,87 @@ const updateFavorites = (req, res) => {
     });
 };
 
+const updatePlayedSongs = (req, res) => {
+    let userId = req.params.userId;
+    let  { currentSong } = req.body; 
+    let songId = currentSong[0] + "&" + currentSong[1];
+    let songName = currentSong[0];
+    let songAuthor = currentSong[1];
+    let songAlbum = currentSong[2];
+    let songCover = currentSong[3];
+    let score = currentSong[4];
+
+    const databaseConnection = getConnection();
+    databaseConnection.collection("users").findOne({"userId": userId}, { projection: { _id:0 } }, 
+        function(error, data) {
+            if (error) {
+                res.status(400).send('⛔️ An error occurred getting single users ... \n[Error]: ' + error);
+            } else { 
+                var songs = [];
+                for(var k in data.playedSongs){
+                    songs.push(data.playedSongs[k][0]);
+                }
+                if(songs.includes(songName)){
+                    for(var k in data.playedSongs){
+                        if(data.playedSongs[k][0] === songName){
+                            var timesPlayed = data.playedSongs[k][4] + 1;
+                            data.playedSongs[k][4] = timesPlayed;
+
+                            var updatedData = { $set: { playedSongs: data.playedSongs } };
+
+                            databaseConnection.collection('users').updateOne({'userId': userId},  updatedData, 
+                            function(error) {
+                                if(error) {
+                                    res.status(400).send('⛔️ An error occurred updating played ... \n[Error]: ' + error);  
+                                } else {
+                                     console.log('☑️ [1] My list modified successfully  ... ');
+                                }
+                            });
+
+                        }
+                    }
+
+                } else {
+                    var oldData = data.playedSongs;
+                    var newData = [songName, songAuthor, songAlbum, songCover, 1, score];
+                    oldData.push(newData);
+                    var finalData = { $set: { playedSongs: oldData } };
+
+                    
+                    databaseConnection.collection('users').updateOne({'userId': userId},  finalData, 
+                    function(error) {
+                        if(error) {
+                            res.status(400).send('⛔️ An error occurred updating played ... \n[Error]: ' + error);  
+                        } else {
+                             console.log('☑️ [1] My list modified successfully ... ');
+                        }
+                    });
+                    
+                }
+
+                databaseConnection.collection("Songs").findOne({"id": songId}, { projection: { _id:0 } }, 
+                function(error, data) {
+                    if (error) {
+                        res.status(400).send('⛔️ An error occurred getting single song ... \n[Error]: ' + error);
+                    } else {
+                        var setNewPlayed = data.played + 1;
+                        var played = { $set: { played: setNewPlayed } };
+
+                        databaseConnection.collection('Songs').updateOne({'id': songId},  played, 
+                        function(error) {
+                            if(error) {
+                                res.status(400).send('⛔️ An error occurred updating song ... \n[Error]: ' + error);  
+                            } else {
+                                res.status(200).send('☑️ The song was modified successfully ... ');
+                            }
+                        });
+                    }
+              });
+
+            }
+      });
+};
 
 
-module.exports = { verifyUser, getSingleUser,updateFavorites }
+
+module.exports = { verifyUser, getSingleUser,updateFavorites, updatePlayedSongs }
