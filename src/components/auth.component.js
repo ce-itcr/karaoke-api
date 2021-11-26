79bc3770b77faedf6a88bdf7f81c9d196447d480
angelortizv <angelo2898@gmail.com>
2021-11-26 12:30:24 -0600

    [ADD] User Registration with mailing

--- src/components/auth.component.js
+++ src/components/auth.component.js
@@ -1,6 +1,17 @@
 const { getConnection } = require('../shared/connection');
 const { isCorrectPassword } = require('../shared/auth/auth.functions');
 
+var nodemailer = require('nodemailer');
+let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'noreply.karaoke.v2@gmail.com',
+        pass: 'fqvoceyzfsjnutgk'
+    }
+})
+
 
 const verifyUser = (req, res) => {
     const { userId, password } = req.body;
@@ -21,6 +32,46 @@ const verifyUser = (req, res) => {
             }
         }
     })
+};
+
+const createUser = (req, res) => {
+    let userId = req.params.userId;
+    console.log(userId);
+    console.log(req.body.mail);
+
+    var mailOptions = {
+        from: 'noreply.karaoke.v2@gmail.com',
+        to: req.body.mail,
+        subject: '[KARAOKE V2.0] Inicio de Sesión',
+        text: 'Use las siguientes credenciales para iniciar sesión: \nusername: ' + userId + '\npassword: ' + req.body.password,
+      };
+
+    const databaseConnection = getConnection();
+    databaseConnection.collection("users").findOne({"userId": userId}, { projection: { _id:0 } }, 
+        function(error, data) {
+            if (error) {
+                res.status(400).send('⛔️ An error occurred getting single user ... \n[Error]: ' + error);
+            } else {
+                if(data === null){
+                    databaseConnection.collection('users').insertOne(req.body, (error, data) => {
+                        if(error){
+                            res.status(400).send('⛔️ An error occurred creating the user ... \n[Error]: ' + error);  
+                        } else {
+                            res.status(200).send('☑️ The user was created successfully ... ');
+                            transporter.sendMail(mailOptions, function(error, info){
+                                if (error) {
+                                  console.log(error);
+                                } else {
+                                  console.log('Mail sended: ' + info.response);
+                                }
+                              });                        
+                        }
+                    });
+                } else if(data !== null){
+                    res.status(401).send('⚠️ There is already a user with the given id ...');
+                }
+            }
+      });
 };
 
 const getSingleUser = (req, res) => {
@@ -147,4 +198,4 @@ const updatePlayedSongs = (req, res) => {
 
 
 
-module.exports = { verifyUser, getSingleUser,updateFavorites, updatePlayedSongs }
+module.exports = { verifyUser, getSingleUser, createUser, updateFavorites, updatePlayedSongs }
--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
-const { verifyUser, getSingleUser, updateFavorites, updatePlayedSongs } = require('../components/auth.component')
+const { verifyUser, getSingleUser, updateFavorites, updatePlayedSongs, createUser } = require('../components/auth.component')
 
 var jsonParser = bodyParser.json();
 
 router.post('/login', jsonParser, verifyUser);
+router.post('/signup/:userId', jsonParser, createUser);
 router.get('/user/:userId', getSingleUser);
 router.put('/user/favorites/:userId', jsonParser, updateFavorites);
 router.put('/user/played/:userId', jsonParser, updatePlayedSongs);
